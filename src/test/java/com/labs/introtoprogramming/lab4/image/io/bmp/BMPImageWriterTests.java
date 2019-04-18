3619026c26bab85dd43bcb65223750566e109f3b
Nikita Volobuev <nikitavbv@gmail.com>
2019-04-18 22:53:02 +0300

    feat(image): add tests for image package

---
+++ src/test/java/com/labs/introtoprogramming/lab4/image/io/bmp/BMPImageWriterTests.java
@@ -0,0 +1,16 @@
+package com.labs.introtoprogramming.lab4.image.io.bmp;
+
+import com.labs.introtoprogramming.lab4.image.RGBImage;
+import java.io.ByteArrayOutputStream;
+import org.junit.Test;
+
+public class BMPImageWriterTests {
+
+  @Test
+  public void testImageWrite() {
+    ByteArrayOutputStream output = new ByteArrayOutputStream();
+    BMPImageWriter writer = new BMPImageWriter(output);
+    writer.write(new RGBImage());
+  }
+
+}
