a54a7868542afc3532c3e7b0bf700ae4ca7b5bec
Nikita Volobuev <nikitavbv@gmail.com>
2019-04-18 09:14:11 +0300

    feat(project): initial project setup

---
+++ src/test/java/com/labs/introtoprogramming/lab4/MainTests.java
@@ -0,0 +1,12 @@
+package com.labs.introtoprogramming.lab4;
+
+import org.junit.Test;
+
+public class MainTests {
+
+  @Test
+  public void testMainStarts() {
+    Main.main(new String[]{});
+  }
+
+}
