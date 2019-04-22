c71463eb23dcd0a1a52f63d65966ea8bfbdabc1b
Nikita Volobuev <nikitavbv@gmail.com>
2019-04-22 21:34:54 +0300

    feat(BMPImageWriter): initial implementation

--- src/main/java/com/labs/introtoprogramming/lab4/image/io/ImageWriter.java
+++ src/main/java/com/labs/introtoprogramming/lab4/image/io/ImageWriter.java
@@ -1,9 +1,11 @@
 package com.labs.introtoprogramming.lab4.image.io;
 
 import com.labs.introtoprogramming.lab4.image.RGBImage;
+import java.io.IOException;
 
 public interface ImageWriter {
 
-  public void write(RGBImage image);
+  public void write(RGBImage image) throws IOException;
 
+  void close() throws IOException;
 }
