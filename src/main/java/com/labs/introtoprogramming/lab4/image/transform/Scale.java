c75175c1af6e19158dbaf06b08c21b99f32ad721
Brook333 <maher5ter270301@gmail.com>
2019-04-23 20:52:55 +0300

    refactor(scale): add tests

--- src/main/java/com/labs/introtoprogramming/lab4/image/transform/Scale.java
+++ src/main/java/com/labs/introtoprogramming/lab4/image/transform/Scale.java
@@ -19,28 +19,30 @@ public class Scale implements RGBImageTransformation {
     int scaledWidth = (int) (w * horizontalScale);
     int scaledHeight = (int) (h * verticalScale);
     RGBImage scaledImage = new RGBImage(scaledWidth, scaledHeight);
-    double xRation = (double) (w - 1) / scaledWidth;
-    double yRation = (double) (h - 1) / scaledHeight;
+    double xRation = (double) (w - 1) / (scaledWidth - 1 > 1 ? scaledWidth - 1 : scaledWidth);
+    double yRation = (double) (h - 1) / (scaledHeight - 1 > 1 ? scaledHeight - 1 : scaledHeight);
     for (int i = 0; i < scaledHeight; i++) {
       for (int j = 0; j < scaledWidth; j++) {
         double x = j * xRation;
         double y = i * yRation;
-        int realX = (int) x;
-        int realY = (int) y;
-        Pixel leftUpperCorner = image.getPixel(realX, realY);
-        Pixel rightUpperCorner = image.getPixel(realX + 1, realY);
-        Pixel leftLowerCorner = image.getPixel(realX, realY + 1);
-        Pixel rightLowerCorner = image.getPixel(realX + 1, realY + 1);
-        Pixel xInterpolationUpper = colorLerp(x, realX, realX + 1, leftUpperCorner, rightUpperCorner);
-        Pixel xInterpolationLower = colorLerp(x, realX, realX + 1, leftLowerCorner, rightLowerCorner);
-        Pixel value = colorLerp(y, realY, realY + 1, xInterpolationUpper, xInterpolationLower);
+        int leftX = (int) x;
+        int leftY = (int) y;
+        int rightX = leftX + 1 < w ? leftX + 1 : leftX;
+        int rightY = leftY + 1 < h ? leftY + 1 : leftY;
+        Pixel leftXCorner = image.getPixel(leftX, leftY);
+        Pixel rightXCorner = image.getPixel(rightX, leftY);
+        Pixel leftYCorner = image.getPixel(leftX, rightY);
+        Pixel rightYCorner = image.getPixel(rightX, rightY);
+        Pixel xInterpolationUpper = colorLerp(x, leftX, leftX + 1, leftXCorner, rightXCorner);
+        Pixel xInterpolationLower = colorLerp(x, leftX, leftX + 1, leftYCorner, rightYCorner);
+        Pixel value = colorLerp(y, leftY, leftY + 1, xInterpolationUpper, xInterpolationLower);
         scaledImage.setPixel(j, i, value);
       }
     }
     return scaledImage;
   }
 
-  private Pixel colorLerp(double coord, int coord1, int coord2, Pixel v1, Pixel v2) {
+  Pixel colorLerp(double coord, int coord1, int coord2, Pixel v1, Pixel v2) {
     return new Pixel(
             (byte) lerp(coord, coord1, coord2, toUnsignedInt(v1.red()), toUnsignedInt(v2.red())),
             (byte) lerp(coord, coord1, coord2, toUnsignedInt(v1.green()), toUnsignedInt(v2.green())),
@@ -48,11 +50,11 @@ public class Scale implements RGBImageTransformation {
     );
   }
 
-  private int toUnsignedInt(byte b) {
+  int toUnsignedInt(byte b) {
     return (b + 256) % 256;
   }
 
-  private double lerp(double coord, int coord1, int coord2, int v1, int v2) {
+  double lerp(double coord, int coord1, int coord2, int v1, int v2) {
     return (coord2 - coord) / (coord2 - coord1) * v1 + (coord - coord1) / (coord2 - coord1) * v2;
   }
 }
